dacdc95e90fa5a2bf821081d2e5ea20622e30b9f
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2024-07-26 19:17:46 -0300

    feat: update comments

--- front-end reat/my-first-app/src/App.jsx
+++ front-end reat/my-first-app/src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-// No React devemos importar a imagem antes de utilizar no src da tag
-
 import Navbar from "./components/navbar/Navbar";
 import "./styles/App.css";
 
 class App extends React.Component {
-  // Para renderizar o conteudo HTML utilizamos o Método RENDER();
-
-  // um componente em classe é uma classe que herda a classe Component do React e retorna um HTML dentro do método Render
-
   render() {
     return <Navbar />;
   }
 }
-
 export default App;
+
+// No React devemos importar a imagem antes de utilizar no src da tag
+// Para renderizar o conteudo HTML utilizamos o Método RENDER();
+
+// um componente em classe é uma classe que herda a classe Component do React e retorna um HTML dentro do método Render
