025d4203cc8edcb8bfb9416388a053110e64cfe6
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2024-08-13 11:04:45 -0300

    refactor: add Card component and add styles

--- Projetos/beautyshop/src/Components/PageLayout/PageLayout.tsx
+++ Projetos/beautyshop/src/Components/PageLayout/PageLayout.tsx
@@ -1,3 +1,4 @@
+import { ProductCard } from "../ProductCard/ProductCard";
 import * as s from "./pageLayoutStyle";
 
 export const PageLayout: React.FC = () => {
@@ -5,15 +6,7 @@ export const PageLayout: React.FC = () => {
     <div>
       <s.PageContainer>
         <s.CardContainer>
-          <s.Card>
-            <img
-              src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png"
-              alt=""
-            />
-            <s.ProductTitle>Title</s.ProductTitle>
-            <s.Category>cat 1 cat 2</s.Category>
-            <s.btnAddCart>Add to Cart</s.btnAddCart>
-          </s.Card>
+         <ProductCard/>
         </s.CardContainer>
       </s.PageContainer>
     </div>
--- Projetos/beautyshop/src/Components/PageLayout/pageLayoutStyle.ts
+++ Projetos/beautyshop/src/Components/PageLayout/pageLayoutStyle.ts
@@ -16,49 +16,4 @@ export const CardContainer = styled.section`
   margin-top: 5px;
   padding: 25px;
   border-radius: 5px;
-  
-`;
-
-export const Card = styled.article`
-  min-width: 230px;
-  min-height: 350px;
-  background-color: white;
-  color: black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 40px;
-  font-weight: 600;
-  flex-direction: column;
-  border-radius: 5px;
-  box-shadow: 2px 2px 12px pink;
-  img {
-    width: 230px;
-    height: 220px;
-    background-color: white;
-    border-radius: 5px;
-  }
-`;
-
-export const ProductTitle = styled.h3`
-  font-family: "Dancing Script", cursive;
-  font-size: 26px;
-  margin-top: 5px;
-`;
-export const Category = styled.span`
-  margin-top: -10px;
-  font-size: 20px;
-  padding: 10px;
-`;
-export const btnAddCart = styled.button`
-  width: 210px;
-  padding: 10px 5px;
-  border: none;
-  background-color: pink;
-  color: gray;
-  font-weight: 600;
-  font-family: inherit;
-  font-size: 17px;
-  border-radius: 10px;
-  cursor: pointer;
 `;
